ad402bd63942c6cb288f87fe81704d17ecf82a69
Максим Клячин <m.kliachin@ente-ltd.ru>
2025-03-24 16:13:30 +0500

    rollback

--- mongo-sharding-repl/scripts/mongo-init.sh
+++ mongo-sharding-repl/scripts/mongo-init.sh
@@ -16,16 +16,16 @@ docker compose exec -it shard1-r1 mongosh --port 27011 --quiet <<EOF
 rs.initiate(
     {_id: "shard1", members: [
         {_id: 1, host: "shard1-r1:27011"},
-        {_id: 2, host: "shard1-r2:27011"},
-        {_id: 3, host: "shard1-r3:27011"}
+        {_id: 2, host: "shard1-r2:27012"},
+        {_id: 3, host: "shard1-r3:27013"}
     ]}) 
 EOF
 
 docker compose exec -it shard2-r1 mongosh --port 27014 --quiet <<EOF
 rs.initiate({_id: "shard2", members: [
     {_id: 4, host: "shard2-r1:27014"},
-    {_id: 5, host: "shard2-r2:27014"},
-    {_id: 6, host: "shard2-r3:27014"}
+    {_id: 5, host: "shard2-r2:27015"},
+    {_id: 6, host: "shard2-r3:27016"}
 ]}) 
 EOF
 
